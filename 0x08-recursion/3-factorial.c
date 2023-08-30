e69eb6eb6faed19811ee88e5304271fa830eb8bc
Jeff Kirui <kipkoech_g@yahoo.com>
2023-08-30 12:33:30 +0300

    Task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * factorial - function that returns the factorial of a given number
+ * @n: integer
+ * Return: factorial of a given number
+ */
+int factorial(int n)
+{
+	int result;
+
+	if (n < 0)
+		return (-1);
+	else if (n == 1 || n == 0)
+		result = 1;
+	else
+		result = n * factorial(n - 1);
+
+	return (result);
+
+}
