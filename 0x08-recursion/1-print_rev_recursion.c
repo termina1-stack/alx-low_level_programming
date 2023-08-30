1c32c24330c1d9b89f42265ab5919ed358d7eb0e
Jeff Kirui <kipkoech_g@yahoo.com>
2023-08-30 11:34:36 +0300

    Task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s: takes in a string to be reversed
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
