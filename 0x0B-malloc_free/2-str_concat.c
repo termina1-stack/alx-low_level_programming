589f127ef2171140e64f45571e3f02e4bc052709
Jeff Kirui <kipkoech_g@yahoo.com>
2023-09-05 17:44:04 +0300

    Fixing bug in task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -25,6 +25,6 @@ char *str_concat(char *s1, char *s2)
 	for (index = 0; s1[index]; index++)
 		join_str[concat_index++] = s1[index];
 	for (index = 0; s2[index]; index++)
-		join_str[concat_indeix++] = s2[index];
+		join_str[concat_index++] = s2[index];
 	return (join_str);
 }
